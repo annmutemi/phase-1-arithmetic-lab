00cd15e8c57146a6e339f1490e28b312aa17381c
annmutemi <annshiru92@gmail.com>
2024-06-29 15:45:38 +0300

    Arithetics oparations

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 
 // Write your code here
+let num1 = 31
+let num2 = 2
+let multiply = num1 * num2
+
+
+let random = Math.ceil(Math.random() * 10)
+console.log(random)
+
+let num3 = 21
+let num4 = 17
+let mod = num3 % num4
+console.log(`remainder ${mod}`)
+
+
+let max = Math.max(1,2,3,4,5,6,7,8,9,20)
